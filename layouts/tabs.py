b28f366d246227f08c0b436b162817743ad9f572
Pooya Shirazi <p.shirazi.a@gmail.com>
2020-12-13 19:15:51 +0330

    Add Data

---
+++ layouts/tabs.py
@@ -0,0 +1,36 @@
+import dash_html_components as html
+
+# sidebar_tab1 =
+#
+#
+# body_tab1 =
+#
+#
+# main_tab1 =
+
+
+tab1 = html.Div(
+    className='containerTab1',
+    children=[
+        html.Main(
+            className='mainTb1',
+            children=[]
+        ),
+        html.Div(
+            className='sidebarTab1',
+            children=[]
+        ),
+        html.Div(
+            className='content1',
+            children=[]
+        ),
+        html.Div(
+            className='content2',
+            children=[]
+        ),
+        html.Div(
+            className='content3',
+            children=[]
+        )
+    ]
+)
